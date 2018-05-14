d5209dbd2e54daf16d9f83bdd98d111cb080d26f
judasane <judasane@gmail.com>
2018-05-14 06:10:03 -0500

    Cambiado el index.php por el original Creada cabecera en cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,10 @@ class Cors
      public function handle($request, Closure $next)
      {
     return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'content-type, withcredentials, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+        ->header('Access-Control-Allow-Headers', 'content-type, withcredentials, Access-Control-Allow-Headers, Origin, Accept, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,x-xsrf-token, x_csrftoken');
+    
       }
 
 }
